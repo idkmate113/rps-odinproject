37f833b4aedcb8519513476a4dff6a332969e3db
id3mate <179154170+idkmate113@users.noreply.github.com>
2025-02-08 08:22:34 +0100

    Implement playRound game logic

--- javascript.js
+++ javascript.js
@@ -9,11 +9,36 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let humanChoice = prompt("Please choose between Rock,Paper,Scissors", "Rock");
-
   humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1);
+  return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {}
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "Rock" && computerChoice === "Scissors") ||
+    (humanChoice === "Paper" && computerChoice === "Rock") ||
+    (humanChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    console.log(`Gratz, You Win! ${humanChoice} beats ${computerChoice}`);
+    console.log(`Human Score Is: ${++humanScore}`);
+  }
+  if (
+    (computerChoice === "Rock" && humanChoice === "Scissors") ||
+    (computerChoice === "Paper" && humanChoice === "Rock") ||
+    (computerChoice === "Scissors" && humanChoice === "Paper")
+  ) {
+    console.log(`Sadge, You Lose! ${computerChoice} beats ${humanChoice}`);
+    console.log(`Computer Score Is: ${++computerScore}`);
+  }
+  if (humanChoice === computerChoice) {
+    console.log(`It's a draw. Nobody wins. ${humanChoice}-${computerChoice}`);
+    console.log(
+      `Player Score Is: ${++humanScore} - Computer Score Is: ${++computerScore}`
+    );
+  }
+}
 
-console.log(getComputerChoice());
-getHumanChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
