299984c97888692458a385778de1bbcc8c242b13
id3mate <179154170+idkmate113@users.noreply.github.com>
2025-02-08 06:48:52 +0100

    Implement getComputerChoice() and getHumanChoice() functions

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let choice = ["Rock", "Paper", "Scissors"];
+  let index = Math.floor(Math.random() * choice.length);
+  return choice[index]; // return choice.at(index) works as well
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt("Please choose between Rock,Paper,Scissors", "Rock");
+}
+
+function playRound(humanChoice, computerChoice) {}
+
+console.log(getComputerChoice());
+getHumanChoice();
