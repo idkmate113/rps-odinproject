8387766435a4b05f81389c0045674299d7e9ef22
id3mate <179154170+idkmate113@users.noreply.github.com>
2025-02-09 08:39:26 +0100

    Finished main game logic. Refactored code a little bit.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
   let choice = ["Rock", "Paper", "Scissors"];
   let index = Math.floor(Math.random() * choice.length);
@@ -13,32 +10,38 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-  if (
-    (humanChoice === "Rock" && computerChoice === "Scissors") ||
-    (humanChoice === "Paper" && computerChoice === "Rock") ||
-    (humanChoice === "Scissors" && computerChoice === "Paper")
-  ) {
-    console.log(`Gratz, You Win! ${humanChoice} beats ${computerChoice}`);
-    console.log(`Human Score Is: ${++humanScore}`);
+function playGame() {
+  function playRound(humanChoice, computerChoice) {
+    let humanScore = 0;
+    let computerScore = 0;
+    if (
+      (humanChoice === "Rock" && computerChoice === "Scissors") ||
+      (humanChoice === "Paper" && computerChoice === "Rock") ||
+      (humanChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+      console.log(`Gratz, You Win! ${humanChoice} beats ${computerChoice}`);
+      console.log(`Human Score Is: ${++humanScore}`);
+    }
+    if (
+      (computerChoice === "Rock" && humanChoice === "Scissors") ||
+      (computerChoice === "Paper" && humanChoice === "Rock") ||
+      (computerChoice === "Scissors" && humanChoice === "Paper")
+    ) {
+      console.log(`Sadge, You Lose! ${computerChoice} beats ${humanChoice}`);
+      console.log(`Computer Score Is: ${++computerScore}`);
+    }
+    if (humanChoice === computerChoice) {
+      console.log(`It's a draw. Nobody wins. ${humanChoice}-${computerChoice}`);
+      console.log(
+        `Player Score Is: ${++humanScore} - Computer Score Is: ${++computerScore}`
+      );
+    }
   }
-  if (
-    (computerChoice === "Rock" && humanChoice === "Scissors") ||
-    (computerChoice === "Paper" && humanChoice === "Rock") ||
-    (computerChoice === "Scissors" && humanChoice === "Paper")
-  ) {
-    console.log(`Sadge, You Lose! ${computerChoice} beats ${humanChoice}`);
-    console.log(`Computer Score Is: ${++computerScore}`);
-  }
-  if (humanChoice === computerChoice) {
-    console.log(`It's a draw. Nobody wins. ${humanChoice}-${computerChoice}`);
-    console.log(
-      `Player Score Is: ${++humanScore} - Computer Score Is: ${++computerScore}`
-    );
-  }
+  let playTheRound = playRound(humanSelection, computerSelection);
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+playGame();
+playGame();
